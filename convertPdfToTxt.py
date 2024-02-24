9fa3efa4edad16c29d6cf60da4df1e360f921002
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2024-02-23 23:43:40 -0300

    Inclusão do tratamento para o campo Lote

--- convertPdfToTxt.py
+++ convertPdfToTxt.py
@@ -22,16 +22,13 @@ def set_number_of_pages():
     valid_pages = total_pages - 2
     return valid_pages
 
-
 def get_number_of_pages():
     return set_number_of_pages()
-
 
 def visitor_body(text, cm, tm, fontDict, fontSize):
     y = tm[5]
     if y > 0 and y < 750:
         parts.append(text)
-
 
 def txt_save():
     numberOfpages = get_number_of_pages()
@@ -44,7 +41,6 @@ def txt_save():
     with open("catalogo.txt", mode='a+', encoding='utf-8') as file:
         file.write(text_body + "\n")
 
-
 def remove_line_break():
     file = open("catalogo.txt", mode="r", encoding="utf-8")
 
@@ -54,15 +50,13 @@ def remove_line_break():
             arq.write('{}'.format(a))
     file.close()
 
-
 def txt_format():
     with open('catalogo_no_linebreak.txt', mode='r', encoding='utf-8') as arq:
-        result = re.split(r'\d{4}\.\d{6}-\d{1}', arq.readline())
+        result = re.split(r',00', arq.readline())
 
         for item in result:
             with open('saida.txt', mode='a+', encoding='utf-8') as arq:
-                arq.write(item + '\n')
-
+                arq.write(item + ',00\n')
 
 def delete_files():
     file_catalog = os.path.isfile('catalogo.txt')
@@ -74,12 +68,10 @@ def delete_files():
     if file_catalog_no_linebreak:
         os.remove('catalogo_no_linebreak.txt')
 
-
 def convert_to_txt():
     txt_save()
     remove_line_break()
     txt_format()
-
 
 def start():
     if pdf_reader:
--- saveDataInDB.py
+++ saveDataInDB.py
@@ -3,25 +3,25 @@
     2. Segregar os campos de cada registro
     3. Registrar os dados no banco
 
-    # Campos para Tabela Lote
-        ID = number/unique
-        LOTE = number/unique * FALTA
-        CONTRATO = number/unique
-        DESCRIÇÃO = str
-        VALOR
-
-            # Campos futuros
-            POSSUI PEDRA
-            PESO DO LOTE
-            FOTO DO LOTE - img
+    # Campos para Tabela Lotes (ID, LOTE, CONTRATO, DESCRIÇÃO, VALOR)
+        # Campos futuros
+        POSSUI PEDRA
+        PESO DO LOTE
+        FOTO DO LOTE - img
 """
 
 import re
 import sqlite3
 
+def set_lote():
+    lote = line.strip()[0:13]
+    return lote
+
+def get_lote():
+    return set_lote()
 
 def set_contract():
-    contract = line.strip()[0:19]
+    contract = line.strip()[13:32]
     return contract
 
 def get_contract():
@@ -41,28 +41,32 @@ def lengh_price():
 
 def set_description():
     price = int(lengh_price())
-    description = line[19:-price]
+    description = line[32:-price]
     return description
 
 def get_description():
     return set_description()
 
 
-
 with open("saida.txt", mode='r', encoding='utf-8') as file:
+    conexao = sqlite3.connect('db-app.db')
+    cursor = conexao.cursor()
+    cursor.execute('''CREATE TABLE lotes (
+                     id        INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                     lote      TEXT    NOT NULL,
+                     contrato  TEXT    NOT NULL,
+                     descricao TEXT    NOT NULL,
+                     valor     TEXT    NOT NULL,
+                     Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);
+                      ''')
 
     for line in file:
-        """print("Contrato: {}".format(get_contract()))
-        print("Preço: {}".format(get_price()))
-        print("Descrição: {}".format(get_description()))"""
-
+        lote = get_lote()
         contract = get_contract()
         description = get_description()
         price = get_price()
 
-        conexao = sqlite3.connect('db-app.db')
-        cursor = conexao.cursor()
-        cursor.execute("INSERT INTO lote (lote, contrato, descricao, valor) values(?, ?, ?, ?)",
-                       ('', contract, description, price))
+        cursor.execute("INSERT INTO lotes (lote, contrato, descricao, valor) values(?, ?, ?, ?)",
+                       (lote, contract, description, price))
         conexao.commit()
     conexao.close()
