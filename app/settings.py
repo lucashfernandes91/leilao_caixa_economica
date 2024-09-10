62d4b124964dce07e1f31931d54a5f6999e7e6fe
Lucas Fernandes <lucashfernandes@yahoo.com.br>
2024-09-10 15:49:36 -0300

    Update

--- app/settings.py
+++ app/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gfq5t_sly2ntaa0)z1dhaowwypeofiq^*oimu!88br-w0ie@qh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
